d3e60295b5e91a1a1ddd97b59eeec2d0fc0a7d33
Ragutaro <ragutaro@ceres.dti.ne.jp>
2018-02-25 19:47:19 +0900

    2018/02/25 19:47

--- AddFontName/Main.dfm
+++ AddFontName/Main.dfm
@@ -1,9 +1,10 @@
 object frmMain: TfrmMain
-  Left = 1557
-  Top = 238
+  Left = 323
+  Top = 283
+  BorderIcons = [biSystemMenu]
   Caption = 'Add Fontname'
-  ClientHeight = 191
-  ClientWidth = 322
+  ClientHeight = 368
+  ClientWidth = 581
   Color = clBtnFace
   Font.Charset = SHIFTJIS_CHARSET
   Font.Color = clWindowText
@@ -15,38 +16,194 @@ object frmMain: TfrmMain
   Visible = True
   OnClose = FormClose
   OnCreate = FormCreate
-  DesignSize = (
-    322
-    191)
   PixelsPerInch = 96
   TextHeight = 18
   object lstProcess: TListBox
     Left = 0
     Top = 0
-    Width = 322
-    Height = 153
+    Width = 581
+    Height = 216
     Align = alTop
     Anchors = [akLeft, akTop, akRight, akBottom]
     ItemHeight = 18
     TabOrder = 0
   end
-  object btnExecute: TButton
-    Left = 239
-    Top = 159
-    Width = 75
-    Height = 25
-    Anchors = [akRight, akBottom]
-    Caption = #23455#34892
+  object pagControl: TPageControl
+    Left = 0
+    Top = 219
+    Width = 581
+    Height = 149
+    ActivePage = tabAdd
+    Align = alBottom
     TabOrder = 1
-    OnClick = btnExecuteClick
-  end
-  object cmbFont: TSpTBXFontComboBox
-    Left = 8
-    Top = 159
-    Width = 185
-    Height = 26
-    Anchors = [akLeft, akBottom]
-    ItemHeight = 23
-    TabOrder = 2
+    ExplicitTop = 90
+    ExplicitWidth = 417
+    object tabAdd: TTabSheet
+      Caption = #12501#12457#12531#12488#35373#23450#12398#36861#21152
+      DesignSize = (
+        573
+        116)
+      object Label2: TLabel
+        Left = 10
+        Top = 22
+        Width = 65
+        Height = 18
+        Caption = #12501#12457#12531#12488#21517':'
+      end
+      object Label1: TLabel
+        Left = 10
+        Top = 56
+        Width = 89
+        Height = 18
+        Anchors = [akLeft, akBottom]
+        Caption = #12501#12457#12531#12488#12469#12452#12474':'
+        ExplicitTop = 46
+      end
+      object cmbFont: TSpTBXFontComboBox
+        Left = 112
+        Top = 18
+        Width = 453
+        Height = 26
+        Anchors = [akLeft, akRight, akBottom]
+        ItemHeight = 23
+        TabOrder = 0
+      end
+      object edtFontSize: TEdit
+        Left = 111
+        Top = 52
+        Width = 32
+        Height = 26
+        Alignment = taCenter
+        Anchors = [akLeft, akBottom]
+        NumbersOnly = True
+        TabOrder = 1
+        Text = '12'
+      end
+      object btnExecute: TButton
+        Left = 490
+        Top = 86
+        Width = 75
+        Height = 25
+        Anchors = [akRight, akBottom]
+        Caption = #23455#34892
+        TabOrder = 2
+        OnClick = btnExecuteClick
+      end
+    end
+    object tabEdit: TTabSheet
+      Caption = #12501#12457#12531#12488#35373#23450#12398#22793#26356
+      ImageIndex = 1
+      DesignSize = (
+        573
+        116)
+      object Button1: TButton
+        Left = 490
+        Top = 86
+        Width = 75
+        Height = 25
+        Anchors = [akTop, akRight]
+        Caption = #23455#34892
+        TabOrder = 0
+        OnClick = Button1Click
+      end
+      object grpFontName: TGroupBox
+        Left = 3
+        Top = 6
+        Width = 325
+        Height = 107
+        TabOrder = 1
+        DesignSize = (
+          325
+          107)
+        object Label5: TLabel
+          Left = 14
+          Top = 32
+          Width = 41
+          Height = 18
+          Caption = #32622#25563#21069':'
+        end
+        object Label6: TLabel
+          Left = 14
+          Top = 68
+          Width = 41
+          Height = 18
+          Caption = #32622#25563#24460':'
+        end
+        object cmbFontBefore: TSpTBXFontComboBox
+          Left = 61
+          Top = 28
+          Width = 256
+          Height = 26
+          Anchors = [akLeft, akRight, akBottom]
+          Enabled = False
+          ItemHeight = 23
+          TabOrder = 0
+        end
+        object cmbFontAfter: TSpTBXFontComboBox
+          Left = 61
+          Top = 64
+          Width = 256
+          Height = 26
+          Anchors = [akLeft, akRight, akBottom]
+          ItemHeight = 23
+          TabOrder = 1
+        end
+      end
+      object grpFontSize: TGroupBox
+        Left = 334
+        Top = 6
+        Width = 145
+        Height = 105
+        TabOrder = 2
+        object Label3: TLabel
+          Left = 14
+          Top = 32
+          Width = 41
+          Height = 18
+          Caption = #32622#25563#21069':'
+        end
+        object Label4: TLabel
+          Left = 14
+          Top = 68
+          Width = 41
+          Height = 18
+          Caption = #32622#25563#24460':'
+        end
+        object edtAfter: TEdit
+          Left = 73
+          Top = 64
+          Width = 30
+          Height = 26
+          Alignment = taCenter
+          NumbersOnly = True
+          TabOrder = 0
+        end
+        object edtBefore: TEdit
+          Left = 73
+          Top = 28
+          Width = 30
+          Height = 26
+          Alignment = taCenter
+          NumbersOnly = True
+          TabOrder = 1
+        end
+      end
+      object chkFontName: TCheckBox
+        Left = 17
+        Top = 2
+        Width = 97
+        Height = 17
+        Caption = #12501#12457#12531#12488#21517
+        TabOrder = 3
+      end
+      object chkFontSize: TCheckBox
+        Left = 348
+        Top = -1
+        Width = 105
+        Height = 26
+        Caption = #12501#12457#12531#12488#12469#12452#12474
+        TabOrder = 4
+      end
+    end
   end
 end
--- AddFontName/Main.pas
+++ AddFontName/Main.pas
@@ -7,21 +7,43 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, System.StrUtils, IniFilesDX, System.IOUtils, System.Types,
-  Vcl.Filectrl, Vcl.StdCtrls, SpTBXEditors, SpTBXExtEditors;
+  Vcl.Filectrl, Vcl.StdCtrls, SpTBXEditors, SpTBXExtEditors, Vcl.ComCtrls;
 
 type
   TfrmMain = class(TForm)
     lstProcess: TListBox;
-    btnExecute: TButton;
+    pagControl: TPageControl;
+    tabAdd: TTabSheet;
+    tabEdit: TTabSheet;
+    Label2: TLabel;
     cmbFont: TSpTBXFontComboBox;
+    Label1: TLabel;
+    edtFontSize: TEdit;
+    btnExecute: TButton;
+    Button1: TButton;
+    grpFontName: TGroupBox;
+    Label5: TLabel;
+    Label6: TLabel;
+    cmbFontBefore: TSpTBXFontComboBox;
+    cmbFontAfter: TSpTBXFontComboBox;
+    grpFontSize: TGroupBox;
+    edtAfter: TEdit;
+    edtBefore: TEdit;
+    Label3: TLabel;
+    Label4: TLabel;
+    chkFontName: TCheckBox;
+    chkFontSize: TCheckBox;
     procedure FormClose(Sender: TObject; var Action: TCloseAction);
     procedure FormCreate(Sender: TObject);
     procedure btnExecuteClick(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
   private
     { Private 宣言 }
     procedure _LoadSettings;
     procedure _SaveSettings;
     procedure _Execute;
+    procedure _ExecuteReplace;
+    procedure _LoadXAML;
   public
     { Public 宣言 }
   end;
@@ -42,6 +64,11 @@ begin
   _Execute;
 end;
 
+procedure TfrmMain.Button1Click(Sender: TObject);
+begin
+  _ExecuteReplace;
+end;
+
 procedure TfrmMain.FormClose(Sender: TObject; var Action: TCloseAction);
 begin
   _SaveSettings;
@@ -52,6 +79,7 @@ end;
 procedure TfrmMain.FormCreate(Sender: TObject);
 begin
   _LoadSettings;
+  _LoadXAML;
 end;
 
 procedure TfrmMain._Execute;
@@ -87,9 +115,11 @@ begin
             lstProcess.TopIndex := lstProcess.Items.Count-1;
             Application.ProcessMessages;
             if ContainsText(sl[i], 'FontSize') then
-              sl[i] := ReplaceText(sl[i], '>', Format(' FontFamily="%s">', [cmbFont.Text]))
-            else
-              sl[i] := ReplaceText(sl[i], '>', Format(' FontSize="12" FontFamily="%s">', [cmbFont.Text]));
+            begin
+            	sl[i] := ReplaceText(sl[i], 'FontSize="12"', Format('FontSize="%s"', [edtFontSize.Text]));
+            	sl[i] := ReplaceText(sl[i], '>', Format(' FontFamily="%s">', [cmbFont.Text]));
+            end else
+              sl[i] := ReplaceText(sl[i], '>', Format(' FontSize="%s" FontFamily="%s">', [edtFontSize.Text, cmbFont.Text]));
             Break;
           end;
       end;
@@ -105,6 +135,63 @@ begin
   end;
 end;
 
+procedure TfrmMain._ExecuteReplace;
+var
+  sl : TStringList;
+  ar : TStringDynArray;
+  s : String;
+  i : Integer;
+begin
+  //置換処理は、すでに Font設定が追加されていることが前提となる。
+  ar := TDirectory.GetFiles(GetApplicationPath + 'EpgTimer', '*.xaml', TSearchOption.soAllDirectories);
+  sl := TStringList.Create;
+  try
+    for s in ar do
+    begin
+      lstProcess.Items.Add('open:' + ExtractFileName(s));
+      lstProcess.TopIndex := lstProcess.Items.Count-1;
+      Application.ProcessMessages;
+      sl.LoadFromFile(s, TEncoding.UTF8);
+      try
+        for i := 0 to sl.Count-1 do
+        begin
+          if ContainsText(sl[i], 'FontFamily=') then
+          begin
+            Application.ProcessMessages;
+            //フォント名
+            if chkFontName.Checked then
+            begin
+            	sl[i] := ReplaceText(sl[i],
+                                  Format('FontFamily="%s"', [cmbFontBefore.text]),
+                                  Format('FontFamily="%s"', [cmbFontAfter.text]));
+              lstProcess.Items.Add('changed FontFamily:' + ExtractFileName(s));
+              lstProcess.TopIndex := lstProcess.Items.Count-1;
+            end;
+            //フォントサイズ
+            if chkFontSize.Checked then
+            begin
+            	sl[i] := ReplaceText(sl[i],
+                                  Format('FontSize="%s"', [edtBefore.Text]),
+                                  Format('FontSize="%s"', [edtAfter.Text]));
+              lstProcess.Items.Add('changed FontSize:' + ExtractFileName(s));
+              lstProcess.TopIndex := lstProcess.Items.Count-1;
+            end;
+            Break;
+          end;
+        end;
+      except
+        lstProcess.Items.Add('error:' + s);
+        lstProcess.TopIndex := lstProcess.Items.Count-1;
+        Application.ProcessMessages;
+      end;
+      sl.SaveToFile(s, TEncoding.UTF8);
+    end;
+    lstProcess.Items.Add('終了');
+  finally
+    sl.Free;
+  end;
+end;
+
 procedure TfrmMain._LoadSettings;
 var
   ini : TMemIniFile;
@@ -112,9 +199,37 @@ begin
   ini := TMemIniFile.Create(GetIniFileName, TEncoding.Unicode);
   try
     ini.ReadWindowPosition(Self.Name, Self);
-    cmbFont.Text := ini.ReadString(Self.Name, 'FontName', 'メイリオ');
   finally
     ini.Free;
+  end;
+end;
+
+procedure TfrmMain._LoadXAML;
+var
+  sl : TStringList;
+  sTmp, sName, sSize : String;
+begin
+  sl := TStringList.Create;
+  try
+    sl.LoadFromFile(GetApplicationPath + 'EpgTimer\AddManualAutoAddWindow.xaml', TEncoding.UTF8);
+    sTmp := CopyStr(sl.Text, 'FontSize', '>');
+    if sTmp <> '' then
+    begin
+      SplitStringsToAandB(sTmp, ' ', sSize, sName);
+      sSize := RemoveRight(RemoveLeft(sSize, 10), 1);
+      sName := RemoveRight(RemoveLeft(sName, 12), 1);
+      cmbFont.Text := sName;
+      edtFontSize.Text := sSize;
+      cmbFontBefore.Text := sName;
+      edtBefore.Text := sSize;
+      pagControl.Pages[0].Destroy;
+    end
+    else
+    begin
+      pagControl.Pages[1].Destroy;
+    end;
+  finally
+    sl.Free;
   end;
 end;
 
@@ -125,7 +240,6 @@ begin
   ini := TMemIniFile.Create(GetIniFileName, TEncoding.Unicode);
   try
     ini.WriteWindowPosition(Self.Name, Self);
-    ini.WriteString(Self.Name, 'FontName', cmbFont.Text);
     ini.UpdateFile;
   finally
     ini.Free;
